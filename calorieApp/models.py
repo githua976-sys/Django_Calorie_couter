6da0382598e50cb2c1295acc89e3c8b7e79930a6
githua976sys <githua976@gmail.com>
2026-07-05 19:25:48 +0300

    update model.py

--- calorieApp/models.py
+++ calorieApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    food_name = models.CharField(max_length=100)
+    calories = models.PositiveIntegerField()
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.food_name
