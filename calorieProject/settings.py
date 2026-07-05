0dc3cdb293b6a198c2771ea0359dc4f6c1fcc074
githua976sys <githua976@gmail.com>
2026-07-05 19:46:26 +0300

    update setting.py

--- calorieProject/settings.py
+++ calorieProject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'calorieProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'calories_db',
+        "USER" : "Amos",
+        "PASSWORD": "machwani",
+        "HOST" : "localhost",
+        "PORT" : "5432"
     }
 }
 
